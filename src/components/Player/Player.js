780840686840f1bf9fac7b0aeb901f6fe42cd567
Md. Khayrul Islam <devkhayrul@gmail.com>
2021-06-23 14:30:10 +0600

    api updatedd

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,17 +3,21 @@ import Single from '../../Team/Single/Single';
 import Team from '../../Team/Team';
 import './Player.css'
 import '../../..//node_modules/bootstrap/dist/css/bootstrap.min.css';
+import data from '../../../src/fakeData/fakeData.json';
 
 
 const Player = () => {
     const [players, setPlayers] = useState([]);
     const [count, setCount] = useState([]);
 
-    useEffect(() => {
-        fetch('https://api.mocki.io/v1/92aeedc1')
-            .then(res => res.json())
-            .then(data => { setPlayers(data) })
-    }, [])
+    // useEffect(() => {
+    //     fetch('https://mocki.io/v1/9599fa8d-c11e-4687-bbb6-c1f740776ade')
+    //         .then(res => res.json())
+    //         .then(data => { setPlayers(data) })
+    // }, [])
+    useEffect(()=>{
+        setPlayers(data);
+    },[])
     const addHandlePlayer = (player) => {
         const newPlayer = [...count, player];
         setCount(newPlayer);
